28269438fd331a0e39564b2ffc4207c4924ef654
Diego19890102 <diegomonfort1989@gmail.com>
2022-11-15 21:32:09 +0100

    update
    new button - open.html

--- app.js
+++ app.js
@@ -10,8 +10,24 @@ function loguear() {
         window.location="open.html";
     }
     else {
-        alert("Incorrect Dates")
+        alert("Incorrect Dates");
     }
 }
 
 
+var nueva = null;
+function nuevaventana(){
+    nueva = window.open("","", "resizable, width=300, heigth=300");
+    nueva.document.open();
+    nueva.document.writeIn("<html><head><title>Neues Dokument</title></head></html>");
+
+    nueva.document.writeIn("<body><h1>Das ist wie ein H1</h1></body>");
+
+    nueva.document.writeIn("<img src='img3.jpg' height='15' width='15'>");
+
+    nueva.document.writeIn("<h2>Das ist ein untertitel!</h2>");
+    nueva.document.writeIn("</body></html>");
+
+
+    nueva.document.close();
+}
